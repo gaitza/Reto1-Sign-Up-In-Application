0ec4d822d4a524d3c527c6c892f56621dfe80dad
bayron takeo <bayrontakeoch21@gmail.com>
2023-10-17 12:15:48 +0200

    "LibPhoneNumber implementado y funcionando, Empiezo a implementar el autorellando del email"

--- src/view/SignUpController.java
+++ src/view/SignUpController.java
@@ -17,8 +17,11 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.UnaryOperator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -27,6 +30,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
@@ -42,14 +48,15 @@ public class SignUpController {
     @FXML
     private ComboBox comboPhone;
     @FXML
-    private TextField textFieldPhone;
+    private TextField textFieldPhone, textFieldEmail;
     @FXML
-    private Line lineInvalidPhone;
+    private Line lineInvalidPhone, lineInvalidEmail;
     @FXML
-    private Text labelInvalidPhone;
+    private Text labelInvalidPhone, labelInvalidEmail;
 
     private Map<String, String> prefijosTelefonos;
     private static Map<String, String> acronimos = new HashMap<>();
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     private static final Logger LOGGER = Logger.getLogger("SignUpController.class");
 
@@ -82,9 +89,62 @@ public class SignUpController {
             }
         });
 
+        UnaryOperator<TextFormatter.Change> filter = change -> {
+            String newText = change.getControlNewText();
+            if (newText.endsWith("@h")) {
+                change.setText("hotmail.com");
+                change.setCaretPosition(change.getCaretPosition()  + "otmail.com".length());
+            } else if (newText.endsWith("@g")) {
+                change.setText("gmail.com");
+                change.setCaretPosition(change.getCaretPosition() + "mail.com".length());
+            }
+            return change;
+        };
+
+        TextFormatter<String> textFormatter = new TextFormatter<>(filter);
+        textFieldEmail.setTextFormatter(textFormatter);
+
         textFieldPhone.focusedProperty().addListener(this::cambioDeFoco);
+        textFieldEmail.focusedProperty().addListener(this::cambioDeFoco);
+        textFieldEmail.setOnKeyPressed(this::confirmarEmail);
         stage.show();
         LOGGER.info("SingUp window initialized");
+    }
+
+    private void confirmarEmail(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            textFieldEmail.selectRange(0, 0); // Desseleccionar
+            event.consume(); // Prevenir que se procese el evento de tecla Enter
+        } else if (event.getCode() == KeyCode.BACK_SPACE && textFieldEmail.getSelection() != null) {
+            textFieldEmail.deleteText(textFieldEmail.getSelection());
+        } else if (event.getCode() == KeyCode.BACK_SPACE) {
+            int caretPosition = textFieldEmail.getCaretPosition();
+            textFieldEmail.deleteText(caretPosition - 1, caretPosition);
+        }
+    }
+
+    private boolean emailEsValido() {
+        boolean coincide = false;
+        Pattern pattern = Pattern.compile(emailPattern);
+        Matcher matcher = pattern.matcher(textFieldEmail.getText());
+        if (matcher.find()) {
+            coincide = true;
+        }
+        try {
+            if (!coincide) {
+                throw new IOException("Invalid format of email (*@*.*)");
+            }
+            lineInvalidEmail.setStroke(Color.GREY);
+            labelInvalidEmail.setText("");
+            return true;
+
+        } catch (IOException ex) {
+            LOGGER.info(ex.getMessage());
+            lineInvalidEmail.setStroke(Color.RED);
+            labelInvalidEmail.setText(ex.getMessage());
+            return false;
+        }
+
     }
 
     private void mostrarClaveSeleccionada(String newValue, ComboBox comboPhone) {
@@ -101,7 +161,7 @@ public class SignUpController {
     public static Map<String, String> leerCsv() {
         Map<String, String> datos = new HashMap<>();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\bayro\\Desktop\\Reto1-Sign-Up-In-Application\\src\\resources\\paises.csv"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\2dam\\Desktop\\Reto1-Sign-Up-In-Application\\src\\resources\\paises.csv"))) {
             String linea;
             while ((linea = reader.readLine()) != null) {
                 String[] partes = linea.split(",");
@@ -147,6 +207,10 @@ public class SignUpController {
                 } catch (NumberParseException ex) {
                     Logger.getLogger(SignUpController.class.getName()).log(Level.SEVERE, null, ex);
                 }
+            }
+            if (!textFieldEmail.isFocused()) {
+                boolean valido = emailEsValido();
+
             }
         }
     }
