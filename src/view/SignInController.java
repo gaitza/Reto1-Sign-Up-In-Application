851aa1fa0e78f068115219b9318c0e6f7c9ad4c6
bayro <bayrontakeoch21@gmail.com>
2023-11-01 21:39:20 +0100

    Implemento la nueva logica a SignInController

--- src/view/SignInController.java
+++ src/view/SignInController.java
@@ -5,17 +5,16 @@
  */
 package view;
 
-import exceptions.InvalidEmailValueException;
-import exceptions.InvalidPasswordException;
 import java.io.IOException;
-import java.util.logging.Level;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import javafx.beans.property.ReadOnlyBooleanProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -23,11 +22,9 @@ import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -43,9 +40,9 @@ public class SignInController {
     private static final Logger LOGGER = Logger.getLogger("SignInController.class");
 
     @FXML
-    private TextField textFieldUser, textFieldPassword;
+    private TextField textFieldEmail, textFieldPassword;
     @FXML
-    private PasswordField password;
+    private PasswordField passwordSignIn;
     @FXML
     private Hyperlink hyperLinkSignUp;
     @FXML
@@ -64,11 +61,15 @@ public class SignInController {
     private ImageView imageViewButton;
 
     private Color customColorGreen = Color.web("#14FF0D");
-    
-    private boolean userException = false;
-    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
-     private final ValidationHelper helper = new ValidationHelper();
+    Map<String, Integer> validate = new HashMap<String, Integer>() {
+        {
+            put("textFieldEmail", 0);
+            put("passwordSignIn", 0);
+        }
+    };
+    private final ValidationHelper helper = new ValidationHelper();
+
     public Stage getStage() {
         return stage;
     }
@@ -100,19 +101,16 @@ public class SignInController {
 
         // USERNAME TEXT FIELD //
         // Check if the text changes
-        textFieldUser.setOnKeyTyped(this::textChanged);
+        textFieldEmail.setOnKeyTyped(this::textChanged);
         //Checking focus change in text field
-        textFieldUser.focusedProperty().addListener(this::focusedChange);
-        textFieldUser.setOnKeyTyped(this::updateLabelPassword);
-        textFieldUser.setOnKeyTyped(this::updateLabelUser);
+        textFieldEmail.focusedProperty().addListener(this::focusedChange);
 
         // PASSWORD FIELD //
         // Checking focus change in text field
-        password.setOnKeyReleased(this::copyPassword);
-        password.setOnKeyTyped(this::textChanged);
+        passwordSignIn.setOnKeyReleased(this::copyPassword);
+        passwordSignIn.setOnKeyTyped(this::textChanged);
         //Checking the change of focus in the password field
-        password.focusedProperty().addListener(this::focusedChange);
-        password.setOnKeyTyped(this::updateLabelPassword);
+        passwordSignIn.focusedProperty().addListener(this::focusedChange);
 
         // PASSWORD TEXT FIELD //
         // Checking the change of focus in the text field
@@ -153,73 +151,11 @@ public class SignInController {
      * pressed
      */
     private void handleShowHide(ActionEvent event) {
-        helper.togglePasswordFieldVisibility(buttonShowHide, imageViewButton, password, textFieldPassword);
-    }
-
-    private void updateLabelPassword(KeyEvent event) {
-        try {
-            if (event.getSource() instanceof PasswordField) {
-                PasswordField passw = (PasswordField) event.getSource();
-                if (passw.getText().length() <= 8) {
-                    
-                    throw new InvalidPasswordException("Password must be al least 8 characters long");
-
-                }
-                linePassword.setStroke(customColorGreen);
-                labelInvalidPassword.setText("");
-            }
-            if (event.getSource() instanceof TextField) {
-                TextField textField = (TextField) event.getSource();
-                if (!textField.getText().isEmpty() && !textField.getText().contains(" ")) {
-                    throw new InvalidPasswordException();
-                }
-                if (textField.equals(textFieldPassword) && textField.getText().length() <= 8) {
-                    throw new InvalidPasswordException("Password must be al least 8 characters long");
-                }
-                lineUser.setStroke(customColorGreen);
-                labelInvalidUser.setText("");
-            }
-
-        } catch (InvalidPasswordException ex) {
-            if (ex.getMessage() != null) {
-                linePassword.setStroke(Color.RED);
-                LOGGER.info(ex.getMessage());
-                labelInvalidPassword.setText(ex.getMessage());
-            }
-        }
-    }
-
-    private void updateLabelUser(KeyEvent event) {
-        try {
-            TextField passw = (TextField) event.getSource();
-            boolean match = false;
-            String user = passw.getText();
-            System.out.println(user);
-            if (user.contains(" ")) {
-                throw new InvalidEmailValueException("Username can't contain an empty space");
-            }
-            Pattern pattern = Pattern.compile(emailPattern);
-            Matcher matcher = pattern.matcher(user);
-            if (matcher.find()) {
-                match = true;
-            }
-            if (!match) {
-                throw new InvalidEmailValueException("Invalid format of email (*@*.*)");
-            }
-            userException = false;
-            lineUser.setStroke(Color.GREY);
-            labelInvalidUser.setText("");
-        } catch (InvalidEmailValueException ex) {
-            lineUser.setStroke(Color.RED);
-            LOGGER.info(ex.getMessage());
-            labelInvalidUser.setText(ex.getMessage());
-            userException = true;
-        }
-
+        helper.togglePasswordFieldVisibility(buttonShowHide, imageViewButton, passwordSignIn, textFieldPassword);
     }
 
     private void copyPassword(KeyEvent event) {
-        helper.copyPassword(password, textFieldPassword);
+        helper.copyPassword(passwordSignIn, textFieldPassword);
     }
 
     private void textChanged(KeyEvent event) {
@@ -232,30 +168,18 @@ public class SignInController {
     private void focusedChange(ObservableValue observable, Boolean oldValue, Boolean newValue) {
 
         if (oldValue) {
-            if (!textFieldUser.isFocused() && !userException) {
+            if (oldValue) {
                 try {
-                    if (textFieldUser.getText().isEmpty()) {
-                        throw new IOException("Enter a username");
+                    ReadOnlyBooleanProperty focusedProperty = (ReadOnlyBooleanProperty) observable;
+                    Node node = (Node) focusedProperty.getBean();
+
+                    if (node.getId().equals("textFieldEmail")) {
+                        helper.executeValidations(textFieldEmail.getId(), textFieldEmail.getText(), lineUser, labelInvalidUser, "", validate);
+                    } else if (node.getId().equals("passwordSignIn")) {
+                        helper.executeValidations(passwordSignIn.getId(), passwordSignIn.getText(), linePassword, labelInvalidPassword, "", validate);
                     }
-                    lineUser.setStroke(customColorGreen);
-                    labelInvalidUser.setText("");
-                } catch (IOException ex) {
-                    lineUser.setStroke(Color.RED);
-                    LOGGER.info(ex.getMessage());
-                    labelInvalidUser.setText(ex.getMessage());
-                }
-            }
-            if (!password.isFocused() && !textFieldPassword.isFocused() && !linePassword.getStroke().equals(Color.RED)) {
-                try {
-                    if (password.getText().isEmpty()) {
-                        throw new IOException("Enter a password");
-                    }
-                    linePassword.setStroke(customColorGreen);
-                    labelInvalidPassword.setText("");
-                } catch (IOException ex) {
-                    linePassword.setStroke(Color.RED);
-                    LOGGER.info(ex.getMessage());
-                    labelInvalidPassword.setText(ex.getMessage());
+                } catch (Exception e) {
+
                 }
             }
 
